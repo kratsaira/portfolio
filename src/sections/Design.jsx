ee7038dce40aa5d1f054dc0ab63ed40f5aeb6518
kratsaira <midnightrainn8@gmail.com>
2026-03-17 14:50:28 -0600

    removed a design poster

--- src/sections/Design.jsx
+++ src/sections/Design.jsx
@@ -2,12 +2,10 @@ import "../styles/Design.css";
 import { useState } from "react";
 
 import img1_thumb from "../assets/azul3.webp";
-import img2_thumb from "../assets/icanbethebesttoyou.webp";
 import img3_thumb from "../assets/first_slide.webp";
 import img4_thumb from "../assets/kadermagazine.webp";
 
 import img1_full from "../assets/azul3.webp";
-import img2_full from "../assets/icanbethebesttoyou.webp";
 import img3_full from "../assets/first_slide.webp";
 import img4_full from "../assets/kadermagazine.webp";
 
@@ -16,7 +14,6 @@ function Design() {
 
   const designs = [
     { thumb: img1_thumb, full: img1_full },
-    { thumb: img2_thumb, full: img2_full },
     { thumb: img3_thumb, full: img3_full },
     { thumb: img4_thumb, full: img4_full },
 
